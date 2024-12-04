d60a5823c785b6d5a45aeec8d53abd026995b38c
Нешко Елена <maysterrrrrr@gmail.com>
2024-12-04 21:49:18 +0300

    Программа проходит все тесты

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,7 +1,16 @@
 #include <assert.h>
 #include "str2int.h"
 #include "stdio.h"
+#include <stdlib.h>
 
-int str2int(const char *str) {
+int main()
+{
+    char str[12];
+    char* endptr;
+
+    gets(str);
+    long ans = strtol(str, &endptr, 10);
+    if ((ans > 2147483647) || (ans < -2147483648))
     return 0;
+    printf("%ld", ans);
 }
